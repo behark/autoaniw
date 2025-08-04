c28c009f9159c91278b301fa956e4ad2d4cecb0e
AutoAni <autoani@example.com>
2025-08-04 19:33:01 +0200

    fix: update netlify configuration for correct static file serving

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Navbar from '../src/components/layout/Navbar';
+import Header from '../src/components/layout/Header';
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <Navbar />
+      <Header />
       
       {/* Luxury Hero Section */}
       <section className="hero">
